cebffc7bbce14fee16fa31af349eaceddc660ae1
Sean Carter <scarter0313@gmail.com>
2019-04-19 05:35:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,11 @@
-# Add  code here!
-def prime?(n)
+def prime?(num)
   if num < 0 or num == 0 or num == 1
     return false
-  else 
-    (2..n-1).none? {|divisor| n % divisor == 0}
-    return true
+  else
+    (2..num-1).to_a.all? do |possible_factor|
+      num % possible_factor != 0
+    end
   end
 end
-end
 
-puts prime?
+puts prime?(105557)
