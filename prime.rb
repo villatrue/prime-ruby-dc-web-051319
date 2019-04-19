20e9b2a75d7eee552f22ddaffd32669e315f6002
Sean Carter <scarter0313@gmail.com>
2019-04-19 05:33:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,5 +2,6 @@
 def prime?(n)
   if num < 0 or num == 0 or num == 1
     return false
-  else (2..n-1).none? {|divisor| n % divisor == 0}
+  else 
+    (2..n-1).none? {|divisor| n % divisor == 0}
 end
