72c4294ac267ceabf07ffc501383cca940fbaa84
Sean Carter <scarter0313@gmail.com>
2019-04-19 05:31:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,4 @@
 # Add  code here!
- def is_prime n
-  for d in 2..(n - 1)
-   if (n % d) == 0
-    return false
-   end
-  end
-
-  true
- end
+def prime?(n)
+  (2..n-1).none? {|divisor| n % divisor == 0}
+end
